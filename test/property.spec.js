fd91feb78dc5bbdee1ee235de1dad13d1c440971
MisterTicot <mister.ticot@cosmic.plus>
2020-03-04 17:58:37 +0100

    test Change: improve 'property.spec.js'

--- test/property.spec.js
+++ test/property.spec.js
@@ -9,21 +9,102 @@ describe("my.property", () => {
   let object
   beforeEach(() => object = { foo: "bar" })
 
-  describe("hide()", () => {
-    it("makes a property non-enumerable", () => {
-      expect(Object.keys(object)).toEqual(["foo"])
-      my.hide(object, "foo")
-      expect(Object.keys(object)).toEqual([])
+  describe("setProperty()", () => {
+    it("sets property flags to `true`", () => {
+      my.setProperty(object, "baz", { value: true })
+
+      expect(object).toEqual({ foo: "bar", baz: true })
+      expectWritable(object, "baz")
+      expectConfigurable(object, "baz")
     })
 
-    it("creates non-enumerable properties", () => {
-      my.hide(object, "baz", true)
-      expect(Object.keys(object)).toEqual(["foo"])
-      expect(object.baz).toBe(true)
+    it("preserves previous value & flags", () => {
+      my.setProperty(object, "foo")
 
-      // Properties created this way are intended to be writable, configurable.
-      expect(() => object.baz = false).not.toThrow()
-      expect(() => my.hide(object, "baz")).not.toThrow()
+      expect(object).toEqual({ foo: "bar" })
+      expectWritable(object, "foo")
+      expectConfigurable(object, "foo")
+    })
+  })
+
+  describe("hide()", () => {
+    it("makes a property non-enumerable", () => {
+      const parent = Object.create(object)
+      my.hide(parent, "foo")
+
+      expect(parent).toEqual({})
+      expect(object).toEqual({ foo: "bar" })
+
+      my.hide(object, "foo")
+
+      expect(object).toEqual({})
+      expectWritable(object, "foo")
+      expectConfigurable(object, "foo")
+    })
+
+    it("creates a non-enumerable property", () => {
+      my.hide(object, "baz", true)
+
+      expect(object).toEqual({ foo: "bar" })
+      expect(object.baz).toBe(true)
+      expectWritable(object, "baz")
+      expectConfigurable(object, "baz")
+    })
+  })
+
+  describe("lock()", () => {
+    it("makes a property read-only", () => {
+      my.lock(object, "foo")
+
+      expect(object).toEqual({ foo: "bar" })
+      expectNonWritable(object, "foo")
+      expectNonConfigurable(object, "foo")
+    })
+
+    it("creates a read-only property", () => {
+      my.lock(object, "baz", true)
+
+      expect(object).toEqual({ foo: "bar", baz: true })
+      expectNonWritable(object, "baz")
+      expectNonConfigurable(object, "baz")
+    })
+  })
+
+  describe("hideLock()", () => {
+    it("makes a property read-only, non-enumerable", () => {
+      my.hideLock(object, "foo")
+
+      expect(object).toEqual({})
+      expectNonWritable(object, "foo")
+      expectNonConfigurable(object, "foo")
+    })
+
+    it("creates a read-only, non-enumerable property", () => {
+      my.hideLock(object, "baz", true)
+
+      expect(object).toEqual({ foo: "bar" })
+      expectNonWritable(object, "baz")
+      expectNonConfigurable(object, "baz")
     })
   })
 })
+
+/* Helpers */
+
+function expectWritable (object, key) {
+  expect(() => object[key] = null).not.toThrow()
+}
+
+function expectNonWritable (object, key) {
+  expect(() => object[key] = null).toThrow()
+}
+
+function expectConfigurable (object, key) {
+  expect(() =>
+    my.setProperty(object, key, { configurable: true })
+  ).not.toThrow()
+}
+
+function expectNonConfigurable (object, key) {
+  expect(() => my.setProperty(object, key, { configurable: true })).toThrow()
+}
